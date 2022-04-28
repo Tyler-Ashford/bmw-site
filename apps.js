5be51aea5bd953b303ab587e1bc956ee88713242
Tyler Ashford <tyashford30@gmail.com>
2022-04-28 16:36:45 -0400

    updated app.js

--- apps.js
+++ apps.js
@@ -1,12 +1,19 @@
-let username = prompt("What is your favorite car?");
+'use strict';
 
-let response = prompt( " Do you like the M8 or M5?");
-let answer = prompt(); 
+function favoriteCar();{
+    let username = prompt("What is your favorite car?");{
+    console.log( 'Hi  ' + username);
+    }
+    favoriteCar();
+    let answer = prompt( "Hi "  + username + "  Do you like the M8 or M5?");
+    let submit;
 
 if (answer === "M8") {
-    answer = ("You are a winner bro ")
-}else if(answer === "M5"){
-    ("I see you like baby cars")
+    submit = "You are a winner bro ";
+}else if(answer === "M5") {
+    submit = "I see you like baby cars";
 }else{
- answer = ("why would you want anything else other then a bmw.")
+ submit = "why would you want anything else other then a bmw.";
 }
+    document.write('Hi '+ username + ' ' + submit,);
+    return username
