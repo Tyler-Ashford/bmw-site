0c96aada98f9ab03c2a0a494b270e6b5a2ae033d
Tyler Ashford <tyashford30@gmail.com>
2022-04-28 12:02:32 -0400

    Added app.js file

---
+++ apps.js
@@ -0,0 +1,12 @@
+let username = prompt("What is your favorite car?");
+
+let response = prompt( " Do you like the M8 or M5?");
+let answer = prompt(); 
+
+if (answer === "M8") {
+    answer = ("You are a winner bro ")
+}else if(answer === "M5"){
+    ("I see you like baby cars")
+}else{
+ answer = ("why would you want anything else other then a bmw.")
+}
